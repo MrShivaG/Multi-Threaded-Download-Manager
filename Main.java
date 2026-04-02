0c61d281e0d3e89734655dfac228518ce65ecba8
MrShivaG <shivaahirwar2005@gmail.com>
2026-04-02 11:52:06 +0530

    Added messege for CLI intraction

--- Main.java
+++ Main.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 import Downloader.*;
 public class Main {
     public static void main(String[] args) {
-        Downloader downloader = new Downloader("https://dev.mysql.com/get/Downloads/MySQLInstaller/mysql-installer-web-community-8.0.45.0.msi");
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter 1 to start new thread of Downloading\nEnter 2 to quet");
+            int in = sc.nextInt();
+            if(in ==1){
+                System.out.println("Paste Download URL/Link");
+                String url =sc.next();
+                ThreadMaker tm = new ThreadMaker();
+                tm.StartThread(url);
+            }
+            if (in == 2) {
+                System.out.println("Closing Download Manager Safely");
+                break;
+            }
+
+            
+        }
+        //Downloader downloader = new Downloader("https://dev.mysql.com/get/Downloads/MySQLInstaller/mysql-installer-web-community-8.0.45.0.msi");
     }
 }
